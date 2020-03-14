232a52913c2b7494bcd42ba113bda84c63727d9e
hss <1173091704@qq.com>
2020-03-14 17:52:00 +0800

    选择排序

--- 算法/排序/bubleSort.js
+++ 算法/排序/bubleSort.js
@@ -1,11 +1,12 @@
+
 /* 
+https://www.cnblogs.com/onepixel/articles/7674659.html
 冒泡排序：
 比较两个相邻元素的大小，做位置调整
-
 */
 const arr = [2, 3, 1, 5, 6, 0]
-bubleSort(arr)
-function bubleSort(arr) {
+console.log(bubleSort(arr))
+/* function bubleSort(arr) {
     const len = arr.length
     console.log(len)
     for(let o = len; o >= 2; o--){ //遍历次数 右 --> 左 6、5、4、3、2 直到 o>=2停止
@@ -18,5 +19,30 @@ function bubleSort(arr) {
         }
     }
     return arr
-}
+} */
 // 复杂度：o(n^2) 两层循环
+
+function bubleSort(array) {
+    let len = array.length
+    // 外层循环控制比较次数
+    for (let i = 0; i < len - 1; i++) { //len-1:最后一个数会自动比较
+        // 内层循环进行两两对比
+        for (let j = 0; j < len - 1 - i; j++) {  //len - 1 - i同理
+            console.log('i:' + i, 'jj:' + j)
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]]
+            }
+        }
+    }
+    return array
+}
+
+
+
+
+
+
+
+
+
+
---
+++ 算法/排序/selectSort.js
@@ -0,0 +1,45 @@
+/* 
+    https://www.cnblogs.com/onepixel/articles/7674659.html
+    选择排序 时间复杂度：o(n^2)
+    工作原理：首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，然后，
+    再从剩余未排序元素中继续寻找最小（大）元素，
+    然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕。
+*/
+/*const arr = [2, 3, 1, -1,5, 6, 0]
+console.log(selectSort(arr))
+function selectSort(arr) {
+    let [len,minIndex] = [arr.length,null]
+    for (let i = 0; i < len - 1; i++) {  //len - 1避免重复选择最后一个数
+        minIndex = i  //最小值索引
+        for(let j = i + 1; j < len;j++){ //i + 1避免重复取arr[i]
+            if(arr[j] < arr[minIndex]){
+                minIndex = j   //找出最小值的索引
+            }
+        }
+        // 将取出的最小的数与当前位置做交换
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }   
+    return arr
+} */
+const arr = [
+    { age: 19 },
+    { age: 15 },
+    { age: 23 },
+    { age: 2 },
+    { age: 55 }
+]
+console.log(selectSort(arr))
+function selectSort(arr) {
+    let [len,minIndex] = [arr.length,null]
+    for(let i = 0; i < len - 1; i++){
+        minIndex = i //假设，标记当前的数为最小
+        for(let j = i + 1; j < len; j++){ 
+            //找出真正最小的数
+            if (arr[minIndex].age > arr[j].age){
+                minIndex = j
+            }
+        }
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+    return arr
+}
